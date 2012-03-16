efb774042ecc30c5c0ebe03799e84ad305b81227
Rick Wayne <fewayne@wisc.edu>
2012-03-16 20:36:44 +0000

    Modified via ss132 and spreading-risk examples to be Passenger-y
    git-svn-id: https://svn.soils.wisc.edu/svn_repos/IrrigSched/trunk@189 2fdaf364-40d7-4fdf-9344-105978106ad9

--- rails-app/config/deploy.rb
+++ rails-app/config/deploy.rb
@@ -1,12 +1,12 @@
-set :application, "wimnext2"
-set :repository,  "svn+ssh://wayne@molly.soils.wisc.edu/var/svn/soils_ag_wx/trunk/rails-app"
+set :application, "wisp"
+set :repository,  "https://wayne@molly.soils.wisc.edu/svn_repos/IrrigSched/trunk/rails-app
 set :user, "wayne"
 default_run_options[:pty] = true
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/home/wayne/prj/#{application}"
+set :deploy_to, "/var/www/"
 set :deploy_via, "export"
 set :use_sudo, false
 
@@ -14,6 +14,13 @@ set :use_sudo, false
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "alfi.soils.wisc.edu"
-role :web, "alfi.soils.wisc.edu"
-role :db,  "molly.soils.wisc.edu", :primary => true
+role :app, "wisp.cals.wisc.edu"
+role :web, "wisp.cals.wisc.edu"
+role :db,  "wisp.cals.wisc.edu", :primary => true
+
+namespace :deploy do
+  desc 'Restart the Passenger app'
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
