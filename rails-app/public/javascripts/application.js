790c299a407c4ac6492a89477485540379495713
Rick Wayne <fewayne@wisc.edu>
2012-06-05 11:40:40 +0000

    Finally fixed JQGrid session/user-id problem, via setting up proper authenticity tokens for the Javascript stuff.
    git-svn-id: https://svn.soils.wisc.edu/svn_repos/IrrigSched/trunk@206 2fdaf364-40d7-4fdf-9344-105978106ad9

--- rails-app/public/javascripts/application.js
+++ rails-app/public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// From http://henrik.nyh.se/2008/05/rails-authenticity-token-with-jquery
+$(document).ajaxSend(function(event, request, settings) {
+  if (typeof(AUTH_TOKEN) == "undefined") return;
+  // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
+  settings.data = settings.data || "";
+  settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+});
+
+
