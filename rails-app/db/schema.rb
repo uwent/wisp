daf6e70ca99d20fc0e420fa69729c58033216ef8
Mark McEahern <mark@mceahern.com>
2015-06-05 08:21:25 -0500

    Fix migration to work with current devise

--- rails-app/db/schema.rb
+++ rails-app/db/schema.rb
@@ -154,18 +154,19 @@ ActiveRecord::Schema.define(:version => 20150420201434) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "weather_station_data", :force => true do |t|
