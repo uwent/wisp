b80a6b3cb219ca8f88f3ba91c003c06d504ffed5
Mark McEahern <mark@mceahern.com>
2015-06-14 00:26:35 -0500

    Experimenting with CircleCI artifacts

--- rails-app/test/test_helper.rb
+++ rails-app/test/test_helper.rb
@@ -1,6 +1,6 @@
 if ENV['CIRCLE_ARTIFACTS']
   require 'simplecov'
-  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
   SimpleCov.start 'rails' do
     add_filter 'vendor'
