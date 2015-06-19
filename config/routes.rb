a8c41f3a4188c79ea1a141f9b871901b2326fe37
Mark McEahern <mark@mceahern.com>
2015-06-18 20:04:12 -0500

    Normalize

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,14 @@
 RailsApp::Application.routes.draw do
-  get "omniauth_callbacks/google_oauth2"
+  devise_for :users
 
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-
-  resources :plants
-
+  match '/userguide' => 'wisp#userguide'
   match 'fdw/irrig_only/:id' => 'field_daily_weather#irrig_only'
 
   resources :field_daily_weather
-
-  resources :weather_station_data, :collection => {:post_data => :post}
-
+  resources :irrigation_events
+  resources :plants
+  resources :weather_station_data, collection: { post_data: :post }
   resources :weather_stations
 
-  resources :irrigation_events
-
-  get "sessions/new"
-
-  get "sessions/create"
-
-  get "sessions/destroy"
-
-  match "/userguide" => "wisp#userguide"
-
-  root :to => "wisp#index"
-
-  resource :session
-  match '/signin', :to => 'sessions#signin'
-
-  match ':controller(/:action(/:id(.:format)))'
+  root to: "wisp#index"
 end
