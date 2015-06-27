4f9173100b488be61b744ea09d853cbf8f9d0469
Mark McEahern <mark@mceahern.com>
2015-06-26 21:33:40 -0500

    Convert SoilType test to rspec

--- test/unit/soil_type_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SoilTypeTest < ActiveSupport::TestCase
-  test "default really is sandy loam" do
-    assert_equal('Sandy Loam', SoilType.default_soil_type.name)
-  end
-end
