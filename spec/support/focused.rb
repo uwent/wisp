8bc13b93e4fcabf3d9a86593aebce32440757913
Mark McEahern <mark@mceahern.com>
2015-07-10 16:11:03 -0500

    Ruby hash syntax

--- spec/support/focused.rb
+++ spec/support/focused.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |config|
   config.filter_run focused: true
-  config.alias_example_to :fit, :focused => true
+  config.alias_example_to :fit, focused: true
   config.run_all_when_everything_filtered = true
 end
