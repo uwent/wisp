7ac3c9a8f04b0f52b8ed9d2df7de1b7897922457
Mark McEahern <mark@mceahern.com>
2015-07-23 19:39:11 -0500

    Enable simplecov outside of CircleCI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,12 @@
+require 'simplecov'
+
 if ENV['CIRCLE_ARTIFACTS']
-  require 'simplecov'
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
-  SimpleCov.start 'rails' do
-    add_filter 'vendor'
-  end
+end
+
+SimpleCov.start 'rails' do
+  add_filter 'vendor'
 end
 
 ENV["RAILS_ENV"] = "test"
