47e857db314e2b9f911a63f20a4b1b5a7f29c2f8
Mark McEahern <mark@mceahern.com>
2015-08-04 11:09:16 -0500

    Deprecate weather_stations.pivot_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150717014015) do
+ActiveRecord::Schema.define(:version => 20150804160532) do
 
   create_table "blogs", :force => true do |t|
     t.date     "date"
@@ -188,9 +188,9 @@ ActiveRecord::Schema.define(:version => 20150717014015) do
     t.string   "name"
     t.string   "location"
     t.string   "notes"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "pivot_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.integer  "pivot_id_deleted"
   end
 
 end
