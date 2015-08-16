0bedbdc62fd82bdea7315f8ffc4a91625f497554
Mark McEahern <mark@mceahern.com>
2015-08-16 12:07:12 -0700

    Fix sequences

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150804160532) do
+ActiveRecord::Schema.define(:version => 20150816174018) do
 
   create_table "blogs", :force => true do |t|
     t.date     "date"
