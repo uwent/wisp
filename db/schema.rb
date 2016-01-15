d5c883db985f985d8365eccd3fcd0a2eaefea52d
Mark McEahern <mark@mceahern.com>
2016-01-15 15:23:16 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -165,7 +165,7 @@ ActiveRecord::Schema.define(:version => 20150816174018) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["reset_password_token"], :name => "idx_users_index_users_on_reset_password_token", :unique => true
 
   create_table "weather_station_data", :force => true do |t|
     t.integer  "weather_station_id",   :null => false
