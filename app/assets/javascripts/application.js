36e5dadfedb30b1e81d83d7c15c8e173cc0a96e6
Mark McEahern <mark@mceahern.com>
2016-02-09 21:11:55 -0600

    Move weird hack to asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -311,3 +311,11 @@ jQuery(document).ready(function(){
   $("#date_input").change(changeDate);
 });
 
+if (typeof jQuery != 'undefined') {
+  $(document).ajaxSend(function(event, request, settings) {
+    if (typeof(AUTH_TOKEN) == "undefined") return;
+    // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
+    settings.data = settings.data || "";
+    settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+  });
+}
