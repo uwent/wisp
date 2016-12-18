7176b511a12652399e5ff9bd0e2798d5bf8bd1cb
Allen Ballman <aballman@gmail.com>
2016-12-18 09:57:59 -0500

    Added paginate to users index.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < AuthenticatedController
   before_action :ensure_john!
 
   def index
+    @users = User.order(:email).paginate(page: params[:page], per_page: 30)
   end
 
   private
