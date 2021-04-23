669f4f45b5bd52353946085813d619ebbbf1aee6
Ben Bradford <bradfordium@gmail.com>
2021-04-23 08:53:46 -0500

    update configs for rails 5

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,5 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
+# Load the Rails application.
+require_relative 'application'
 
-# Initialize the rails application
-RailsApp::Application.initialize!
-
-# Monkey-patch the method from the 2dc_jqgrid plugin. Rails has a javascript escaping method that
-# inserts backslashes before single quotes; JQGrid chokes on that char sequence.
-module JqgridJson
-  alias :orig_to_jqgrid_json :to_jqgrid_json
-
-  def to_jqgrid_json(attributes, current_page, per_page, total)
-    orig_to_jqgrid_json(attributes, current_page, per_page, total).gsub("\\\'","'")
-  end
-end
+# Initialize the Rails application.
+Rails.application.initialize!
