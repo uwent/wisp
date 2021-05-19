5dd7d71fe9eb29a75b777d22536cb33c8f28ec54
Ben Bradford <bradfordium@gmail.com>
2021-05-19 15:01:45 -0500

    routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, path: 'wisp'
+  devise_for :users, path: 'wisp' do
+    collection do
+      get 'report_setup'
+    end
+  end
 
+  get '/wisp' => 'welcome#index'
   get '/userguide' => 'welcome#guide'
   get 'fdw/irrig_only/:id' => 'field_daily_weather#irrig_only'
 
@@ -13,7 +18,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :wisp, only: [:index] do
+  resources :wisp, only: [] do
     collection do
       get 'project_status'
       get 'pivot_crop'
