c5694430fe17e2f6aeb2ba22bb59f4d4f50c0187
Ben Bradford <bbradford@wisc.edu>
2024-03-17 13:17:01 -0500

    fix css/scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,8 @@
 //= require_tree .
 
 // if (typeof jQuery != 'undefined') {
-//   $(document).ajaxSend(function(event, request, settings) {
-//     if (typeof(AUTH_TOKEN) == "undefined") return;
+//   $(document).ajaxSend(function (event, request, settings) {
+//     if (typeof (AUTH_TOKEN) == "undefined") return;
 //     // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
 //     settings.data = settings.data || "";
 //     settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
