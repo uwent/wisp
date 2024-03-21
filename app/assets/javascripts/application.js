d9a7bdb7d58f3be04ad5b646e67e6cf705597728
Ben Bradford <bbradford@wisc.edu>
2024-03-20 21:11:48 -0500

    fix jquery urls
    url params were escaped to &amp; messing up params

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,12 @@
 $(() => {
   window.history.replaceState(null, null, window.location.pathname)
 });
+
+function escapeHTML(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
